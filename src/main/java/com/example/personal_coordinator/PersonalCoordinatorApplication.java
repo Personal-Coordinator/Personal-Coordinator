96e3e42caa8f2d0e19b9bec18437023214c42ed2
artem <artemsa223@gmail.com>
2024-03-26 00:09:04 +0000

    add api hello world

---
+++ src/main/java/com/example/personal_coordinator/PersonalCoordinatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.personal_coordinator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalCoordinatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalCoordinatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/personal_coordinator/PersonalCoordinatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.personal_coordinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalCoordinatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
